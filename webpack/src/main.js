9469d9fe7af8d48b66c524f32ae1d5907d765b52
Samuel Adesoga <samuel@thoughstudiong.com>
2017-03-06 22:28:35 +0000

    added router for app

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: App },
+    { path: '*', redirect: '/' }
+  ]
 })
+
+new Vue({router}).$mount('#app')
